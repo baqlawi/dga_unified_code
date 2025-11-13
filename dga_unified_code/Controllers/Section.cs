3c315e17e3ec8d2e73b062b393f2f6af7938df98
Abdulmonaem Albaqlawi <monaemm@ksau-hs.edu.sa>
2025-11-13 12:11:27 +0300

    added initial section controller

---
+++ dga_unified_code/Controllers/Section.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dga_unified_code.Controllers
+{
+    public class Section : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
